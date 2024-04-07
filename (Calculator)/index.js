3c2ba5e123de06ad5edf1197a4a016e2924eb6cf
Asif Haque <153304056+AsifHaqe@users.noreply.github.com>
2024-04-07 22:49:32 +0600

    Calculator
    Using Of Function and Operator

---
+++ (Calculator)/index.js
@@ -0,0 +1,12 @@
+var num1=prompt("Enter first number");
+var num2=prompt("Enter second number");
+
+num1=parseInt(num1,10); /////10mik shonkha///0 theke 9 ja 10 ta //////////
+num2=parseInt(num2,10);
+
+var sum,sub;
+sum= num1 + num2;
+document.write("Total" + sum + "</br>");
+
+sub= num1 - num2;
+document.write("Total" + sub);
