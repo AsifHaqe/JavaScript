751327e636ee0c037a5ba8fd36ab79e8cb057e22
Asif Haque <153304056+AsifHaqe@users.noreply.github.com>
2024-06-13 11:06:20 +0600

    Add files via upload

---
+++ Class 29 (1D Array)/index.js
@@ -0,0 +1,175 @@
+// function heightScore(Score){
+//     var max=Score[0];
+// for(x = 1;x<Score.length ;x++){
+// if (max < Score[x]){
+//     max = Score[x];
+//         }
+//     }
+//     return max;
+// }
+// var Score = [21,25,88,39,98]
+// var maxScore = heightScore(Score)
+// console.log(maxScore);
+/////////////////////////////////////////
+// function TopScore(James) {
+//     var max = James[0];
+//     for(x = 1;x<James.length;x++){
+//         if(max < James[x]){
+//             max = James[x]
+//         }
+//     }
+//     return max
+// }
+// var James = [25,45,65,88,90,32]
+// var heights=TopScore(James)
+// console.log(heights)
+// ////////////////////////////////////
+// function heightScore(Score){
+//     var first = Score[0];
+//     for(x = 1;x <Score.length;x++){
+//         if(first < Score[x]){
+//             first = Score[x]
+//         }
+//     }
+//     return first
+// }
+// var Score=[20,15,35,70,88,98]
+// var Maximum = heightScore(Score);
+// console.log(Maximum);
+// //////////////////////////////////////////
+// function HeightScore(Shawa){
+//     var monekori =Shawa[0];
+//     for (x = 1 ; x < Shawa.length;x++){
+//         if (monekori <Shawa[x]){
+//             monekori = Shawa[x]
+
+//         }
+//     }
+//     return Shawa
+// }
+// var Shawa = [55,102,520,403,650,450,321]
+// var Top=HeightScore(Shawa)
+// console.log(Top)
+/////////////////////////////////
+
+function SummerField (Whatsup){
+    var max = [0]
+    for(i = 1;i < Whatsup.length;i++){
+        if(max < Whatsup[i]){
+            max = Whatsup[i]
+        }
+    }
+    return Whatsup
+}
+var Whatsup = [20,55,99,45,65,12,35,96,50,15]
+var Nice = SummerField(Whatsup)
+console.log(SummerField)
+///////////////////////////////////////////////
+// function Mangrove(Neptune){
+//     var max = Neptune[0];
+//     for(i = 1;i<Neptune.length;i++){
+//         if(max <Neptune[i]){
+//             max = Neptune[i]
+//         }
+//     }
+//     return Neptune
+// }
+// var Tumi = Neptune = [30,10,65,90,80,45,22,56,44,52,44]
+// var Me = Mangrove(Neptune)
+// console.log(Mangrove)
+////////////////////////////////////////////////////
+function Mangrove (Neptune){
+    var max = Neptune[0];
+    for(i = 1;i<Neptune.length;i++){
+        if(max < Neptune[i]){
+            max = Neptune[i]
+        }
+    }
+    return Neptune
+}
+var she = Neptune = [55,60,56,89,45,25,22,55,90,99];
+var him = Mangrove(Neptune);
+////////////////////////////////////////////////
+function Mangrove(Uranas){
+    var max = Uranas[0]
+    for(i=1;i<Uranas.length;i++){
+        if(max < Uranas.length[i]){
+            max = Uranas[i]
+        }
+    }
+}
+Uranas = [66,44,55,888,77,11,33,99,66]
+Mangrove(Uranas)
+
+///////////////////////////////////////////////////
+
+////////////////////////////////////////////////
+function Neptune(Mangrove){
+    var max = Mangrove[0];
+    for(i = 1 ; i<Mangrove;i++){
+        if(max < Mangrove[i]){
+            max = Mangrove[i]
+        }
+    }
+}
+var she = Mangrove[10,15,14,17,18,19,113,11]
+var his = Neptune(Mangrove)
+
+/////////////////////////////////////////////
+function Asif(Paramater){
+    var max = Paramater[0]
+    for(i = 1 ;i < Mangrove ; i++){
+        if(max <Paramater[i]){
+            max = Paramater
+        }
+    }
+}
+Paramater = [50,10,60,80,70,90,99,30,10,60,40]
+//////////////////////////////////////////////////
+function Parameter(Haque){
+    var max = Haque[0]
+    for(i = 0;i<Haque.length;i++){
+        if(max <Haque[i]){
+            max = Haque
+        }
+    }
+}
+Haque = [88,89,87,84,81,82,80,83,86,89,85]
+/////////////////////////////////////////////////
+function getAddition(firstNumber,secondNumber){ 
+    var result; 
+    result = firstNumber + secondNumber; 
+    return result; 
+    } 
+var myResult = getAddition(10,20); 
+alert(myResult);
+////////////////////////////////////////////////////
+function getAddition(firstNumber,SecondNumber){
+    var Result;
+    Result = firstNumber + SecondNumber;
+    return Result
+}
+var myResult = getAddition(10,30)
+alert(myResult)
+//////////////////////////////////////////////////////////
+function getSum(prothomNumber,SecondNumber){
+    var Last;
+    Last = prothomNumber + SecondNumber;
+    return Last
+
+}
+var First = getSum(10,60)
+////////////////////////////////////////////////////////
+function Keyword(firstNumber,SecondNumber){
+    var Result; 
+    Result = firstNumber + SecondNumber
+    return Result
+
+}
+var What = Keyword(60,80);
+/////////////////////////////////////////////////////////
+function Lion(parameter1,parameter2){
+    var Finish =parameter1 + parameter2
+    return Finish
+}
+var heh = Lion(30 , 20)
